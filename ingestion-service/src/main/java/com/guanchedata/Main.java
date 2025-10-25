a21cb30cb13909057e8af01db8357d14334eab7d
Pablo <pabcablan2005@gmail.com>
2025-10-25 21:34:01 +0100

    feat: logs applied to controller and separated Main Class

--- ingestion-service/src/main/java/com/guanchedata/Main.java
+++ ingestion-service/src/main/java/com/guanchedata/Main.java
@@ -1,28 +1,24 @@
 package com.guanchedata;
 
+import com.guanchedata.application.usecases.ingestionservice.BookProviderController;
 import com.guanchedata.infrastructure.adapters.*;
 import com.guanchedata.infrastructure.ports.PathGenerator;
-import org.jsoup.Connection;
-
-import java.io.IOException;
+import io.javalin.Javalin;
 import java.nio.file.Paths;
 
 public class Main {
-    public static void main(String[] args) {
-        GutenbergConnection connection = new GutenbergConnection();
-        GutenbergFetch fetch = new GutenbergFetch();
-        GutenbergBookContentSeparator separator = new GutenbergBookContentSeparator();
-
+    public static void main(String[] args){
         PathGenerator pathGenerator = new DateTimePathGenerator(Paths.get(args[0]));
-
+        GutenbergBookContentSeparator separator = new GutenbergBookContentSeparator();
         BookStorageDate storageDate = new BookStorageDate(pathGenerator, separator);
+        BookDownloadLog bookDownloadLog = new BookDownloadLog(args[1]);
+        BookProviderController controller = new BookProviderController(storageDate, bookDownloadLog);
 
-        try {
-            Connection conn = connection.createConnection(1);
-            String response = fetch.fetchBook(conn);
-            storageDate.save(1, response);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        Javalin app = Javalin.create(config -> {
+            config.http.defaultContentType = "application/json";}).start(7001);
+
+        app.post("/ingest/{book_id}",controller::ingestBook);
+        app.get("/ingest/status/{book_id}", controller::status);
+        app.get("/ingest/list", controller::listAllBooks);
     }
 }
--- ingestion-service/src/main/java/com/guanchedata/application/usecases/ingestionservice/BookProviderController.java
+++ ingestion-service/src/main/java/com/guanchedata/application/usecases/ingestionservice/BookProviderController.java
@@ -1,14 +1,13 @@
-package com.guanchedata.application.usecases;
+package com.guanchedata.application.usecases.ingestionservice;
 
 import com.google.gson.Gson;
 import com.guanchedata.infrastructure.adapters.*;
-import com.guanchedata.infrastructure.ports.PathGenerator;
-import io.javalin.Javalin;
 import io.javalin.http.Context;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.List;
 import java.util.Map;
 
@@ -16,32 +15,21 @@ public class BookProviderController {
     private final BookStorageDate storageDate;
     private final BookDownloadLog bookDownloadLog;
     private static final Gson gson = new Gson();
+    private static final Logger log = LoggerFactory.getLogger(BookProviderController.class);
 
     public BookProviderController(BookStorageDate storageDate, BookDownloadLog bookDownloadLog) {
         this.storageDate = storageDate;
         this.bookDownloadLog = bookDownloadLog;
     }
 
-    public static void main(String[] args){
-        PathGenerator pathGenerator = new DateTimePathGenerator(Paths.get(args[0]));
-        GutenbergBookContentSeparator separator = new GutenbergBookContentSeparator();
-        BookStorageDate storageDate = new BookStorageDate(pathGenerator, separator);
-        BookDownloadLog bookDownloadLog = new BookDownloadLog(args[1]);
-        BookProviderController controller = new BookProviderController(storageDate, bookDownloadLog);
-
-        Javalin app = Javalin.create(config -> {
-            config.http.defaultContentType = "application/json";}).start(7001);
-
-        app.post("/ingest/{book_id}",controller::ingestBook);
-        app.get("/ingest/status/{book_id}", controller::status);
-        app.get("/ingest/list", controller::listAllBooks);
-    }
-
-    private void status(Context ctx) throws IOException {
+    public void status(Context ctx) throws IOException {
         int bookId = Integer.parseInt(ctx.pathParam("book_id"));
+        log.info("status() - Start execution for bookId={}", bookId);
         boolean isBookAvailable = bookDownloadLog.isDownloaded(bookId);
 
+
         if (isBookAvailable){
+            log.info("status() - Book {} is available in datalake", bookId);
             Map<String, Object> response = Map.of(
                     "book_id", bookId,
                     "status", "available"
@@ -49,6 +37,8 @@ public class BookProviderController {
 
             ctx.result(gson.toJson(response));
         } else {
+            log.warn("status() - Book {} is not available in datalake", bookId);
+
             Map<String, Object> response = Map.of(
                     "book_id", bookId,
                     "status", "not_available"
@@ -56,22 +46,27 @@ public class BookProviderController {
 
             ctx.result(gson.toJson(response));
         }
+        log.info("status() - Finished execution for bookId={}", bookId);
     }
 
-    private void listAllBooks(Context ctx) throws IOException {
+    public void listAllBooks(Context ctx) throws IOException {
+        log.info("listAllBooks() - List of books in the datalake");
         List<Integer> downloadedBooks = bookDownloadLog.getAllDownloaded();
         Map<String, Object> response = Map.of(
                 "count", downloadedBooks.size(),
                 "books", downloadedBooks
         );
         ctx.result(gson.toJson(response));
+        log.info("listAllBooks() - Finished execution for the list of books");
     }
 
-    private void ingestBook(Context ctx) {
+    public void ingestBook(Context ctx) {
         int bookId = Integer.parseInt(ctx.pathParam("book_id"));
-        try{
+        log.info("ingestBook() - Start processing bookId={}", bookId);
 
+        try {
             if (bookDownloadLog.isDownloaded(bookId)) {
+                log.warn("ingestBook() - Book {} is already downloaded, skipping ingestion", bookId);
                 Map<String, Object> responseAPI = Map.of(
                         "book_id", bookId,
                         "status", "already_downloaded",
@@ -81,21 +76,25 @@ public class BookProviderController {
                 return;
             }
 
-
             GutenbergConnection connection = new GutenbergConnection();
             GutenbergFetch fetch = new GutenbergFetch();
             String response = fetch.fetchBook(connection.createConnection(bookId));
             Path savedPath = storageDate.save(bookId, response);
             bookDownloadLog.registerDownload(bookId);
 
+            log.info("ingestBook() - Book {} downloaded and saved at {}", bookId, savedPath);
+
             Map<String, Object> responseAPI = Map.of(
                     "book_id", bookId,
                     "status", "downloaded",
-                    "path", savedPath.toString()
+                    "path", savedPath.toString().replace("\\", "/")
             );
 
             ctx.result(gson.toJson(responseAPI));
+
         } catch (Exception e) {
+            log.error("ingestBook() - Error processing bookId {}: {}", bookId, e.getMessage(), e);
+
             Map<String, Object> responseError = Map.of(
                     "book_id", bookId,
                     "status", "error",
@@ -103,6 +102,8 @@ public class BookProviderController {
             );
 
             ctx.result(gson.toJson(responseError));
+        } finally {
+            log.info("ingestBook() - Finished processing bookId={}", bookId);
         }
     }
 }
